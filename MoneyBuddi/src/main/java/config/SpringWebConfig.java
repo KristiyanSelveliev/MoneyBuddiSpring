2e10c807c6ffa1115900658d8c9dcaa07755ac6f
KristiyanSelveliev <kriselveliev@abv.bg>
2018-04-25 21:11:52 +0300

    removal

--- MoneyBuddi/src/main/java/config/SpringWebConfig.java
+++ MoneyBuddi/src/main/java/config/SpringWebConfig.java
@@ -1,4 +1,4 @@
-package com.example;
+package config;
 
 import java.util.Locale;
 
--- MoneyBuddi/src/main/java/config/WebInitializer.java
+++ MoneyBuddi/src/main/java/config/WebInitializer.java
@@ -1,4 +1,4 @@
-package com.example;
+package config;
 
 import org.springframework.web.servlet.support.
 AbstractAnnotationConfigDispatcherServletInitializer;
