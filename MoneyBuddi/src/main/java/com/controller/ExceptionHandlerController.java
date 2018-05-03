cb34bbfba148a80b23633235d20b772eee1473a1
KristiyanSelveliev <kriselveliev@abv.bg>
2018-05-03 23:26:12 +0300

    New handler

--- MoneyBuddi/src/main/java/com/controller/ExceptionHandlerController.java
+++ MoneyBuddi/src/main/java/com/controller/ExceptionHandlerController.java
@@ -2,12 +2,25 @@ package com.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
-
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.exceptions.InvalidDataException;
+
 @ControllerAdvice
 public class ExceptionHandlerController {
+	
+	
+	@ExceptionHandler(value=TypeMismatchException.class)
+	public String valueError(HttpServletRequest request,TypeMismatchException e) {
+		e.printStackTrace();
+		InvalidDataException ex=new InvalidDataException("You probably entered wrong value");
+		request.setAttribute("exception",ex);
+		return "error";
+	}
+	
+	
 	
 	@ExceptionHandler(value=Exception.class)
 	public String error(HttpServletRequest request,Exception e) {
